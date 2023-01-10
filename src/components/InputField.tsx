1ad0a70145c6c72da1eb7c73053e277df5ebddcc
Adel <adel.mouhandes@gmail.com>
2023-01-10 12:53:59 +0100

    - implemented the input field

---
+++ src/components/InputField.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './styles.css';
+
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputField = ({todo, setTodo}: Props) => {
+  return (
+    <form className='input'>
+        <input type='input' 
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+        placeholder='Exter a tasks' 
+        className='input_box'/>
+        <button type='submit' className='input_submit'>Go</button>
+    </form>
+  )
+}
+
+export default InputField
